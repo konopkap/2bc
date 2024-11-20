e4fc7b479ac68d7f52454f84f8ef1ac669dc5f81
konopkp <konopka.patryk2@gmail.com>
2024-11-20 12:58:32 +0100

    Empty commit

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -6,7 +6,7 @@ resource "azurerm_dns_zone" "dns" {
 resource "azurerm_dns_a_record" "ingres_nginx" {
   zone_name           = azurerm_dns_zone.dns.name
   name                = "ingress-nginx"
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.resource_group_name 
 
   ttl     = 300
   records = [azurerm_public_ip.pip_ingress.ip_address]
